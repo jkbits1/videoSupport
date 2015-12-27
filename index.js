ae03f37346c216859f6ec3bb3d305e5ce4dc1207
jkbits1 <jkbits1@users.noreply.github.com>
2015-12-27 01:05:23 +0000

    calls async method

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 /**
  * Created by jk on 27/12/15.
  */
+
+var getVideoDiscover = require('./vidDiscover');
+
+var fileName = process.argv[2];
+
+getVideoDiscover(fileName, function (titleDetails) {
+  console.log(titleDetails);
+});
